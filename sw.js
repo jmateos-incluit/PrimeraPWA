13838fd1a2b0db73b8af50c45576c98c634a580b
jmateos-incluit <joaquin.mateos@incluit.com>
2023-09-27 11:52:19 -0300

    .

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 const CACHE_NAME = 'mi-cache'
-const CACHE_VERSION = 'v2'
+const CACHE_VERSION = 'v3'
 
 const filesToCache = [
   '/index.html', // Archivos HTML
-  '/styles.css', // Archivos CSS
+  '/style.css', // Archivos CSS
   '/script.js', // Archivos JavaScript
   '/img/imgpng' // Imágenes
 ]
@@ -38,5 +38,31 @@ self.addEventListener('fetch', function (event) {
       }
       return fetch(event.request)
     })
+  )
+})
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    fetch(event.request)
+      .then(function (response) {
+        // Si la solicitud se completó con éxito (hay conexión)
+        // Aquí puedes personalizar la lógica según tus necesidades
+        if (response.status === 200) {
+          // Realiza alguna acción si la conexión se restaura (opcional)
+          self.registration.showNotification(
+            'Conexión restaurada',
+            'La conexión a Internet se ha restablecido.'
+          )
+        }
+        return response
+      })
+      .catch(function () {
+        // Si la solicitud falla (no hay conexión)
+        // Aquí puedes personalizar la lógica según tus necesidades
+        self.registration.showNotification(
+          'Conexión perdida',
+          'La conexión a Internet se ha perdido.'
+        )
+      })
   )
 })
